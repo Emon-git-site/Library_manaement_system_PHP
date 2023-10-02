8cfa371ed42d582821c02d88b93a3d2546b2ded1
Emon-git-site <136096636+Emon-git-site@users.noreply.github.com>
2023-10-02 08:21:09 +0600

    after t 7

---
+++ student/footer.php
@@ -0,0 +1,31 @@
+                <!-- =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= -->
+                </div>
+            
+            <!--scroll to top-->
+            <a href="#" class="scroll-to-top"><i class="fa fa-angle-double-up"></i></a>
+        </div>
+    </div>
+    <!--BASIC scripts-->
+    <!-- ========================================================= -->
+    <script src="../assets/vendor/jquery/jquery-1.12.3.min.js"></script>
+    <script src="../assets/vendor/bootstrap/js/bootstrap.min.js"></script>
+    <script src="../assets/vendor/nano-scroller/nano-scroller.js"></script>
+    <!--TEMPLATE ../assets/scripts-->
+    <!-- ========../assets/================================================= -->
+    <script src="../assets/javascripts/template-script.min.js"></script>
+    <script src="../assets/javascripts/template-init.min.js"></script>
+    <!-- SECTION ../assets/script and examples-->
+    <!-- ========../assets/================================================= -->
+    <!--Notificat../assets/ion msj-->
+    <script src="../assets/vendor/toastr/toastr.min.js"></script>
+    <!--morris ch../assets/art-->
+    <script src="../assets/vendor/chart-js/chart.min.js"></script>
+    <!--Gallery w../assets/ith Magnific popup-->
+    <script src="../assets/vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+    <!--Examples-../assets/->
+    <script src="../assets/javascripts/examples/dashboard.js"></script>
+</body>
+
+
+
+</html>
--- student/test.php
+++ student/test.php
@@ -1,27 +1,26 @@
-
 <?php
-require_once 'header.php';
+ require_once 'header.php';
 ?>
-
-                <!-- content HEADER -->
+ <!-- content HEADER -->
                 <!-- ========================================================= -->
                 <div class="content-header">
                     <!-- leftside content header -->
                     <div class="leftside-content-header">
                         <ul class="breadcrumbs">
                             <li><i class="fa fa-home" aria-hidden="true"></i><a href="#">Dashboard</a></li>
+                            <li><a href="#">Test</a></li>
                         </ul>
                     </div>
                 </div>
                 <!-- =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= -->
-                <d iv class="row animated fadeInUp">
+                <div class="row animated fadeInUp">
                     <div class="col-sm-12">
                         test
                     </div>
                 
-                </d>
+                </div>
 
-                
+
 <?php
-require_once 'footer.php';
+ require_once 'footer.php';
 ?>
