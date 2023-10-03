cf833c96b866a8edbe9e4f8a5017d91f9bb9d0fd
Emon-git-site <136096636+Emon-git-site@users.noreply.github.com>
2023-10-03 15:49:51 +0600

    after t17

--- librarian/manage_book.php
+++ librarian/manage_book.php
@@ -43,16 +43,21 @@ require_once 'header.php';
 
                             ?>
                                 <tr>
-                                   <td><?= $row['book_name'] ?></td>
-                                   <td><img src="../images/books/<?= $row['book_image'] ?>" width="100px" alt=""></td>
-                                   <td><?= $row['book_author_name'] ?></td>
-                                   <td><?= $row['book_publication_name'] ?></td>
-                                   <td><?= date('d-M-Y', strtotime($row['book_purchase_date'])) ?></td>
-                                   <td><?= $row['book_price'] ?></td>
-                                   <td><?= $row['book_qty'] ?></td>
-                                   <td><?= $row['available_qty'] ?></td>
-                                   <td>1</td>
-                                   
+                                    <td><?= $row['book_name'] ?></td>
+                                    <td><img src="../images/books/<?= $row['book_image'] ?>" width="100px" alt=""></td>
+                                    <td><?= $row['book_author_name'] ?></td>
+                                    <td><?= $row['book_publication_name'] ?></td>
+                                    <td><?= date('d-M-Y', strtotime($row['book_purchase_date'])) ?></td>
+                                    <td><?= $row['book_price'] ?></td>
+                                    <td><?= $row['book_qty'] ?></td>
+                                    <td><?= $row['available_qty'] ?></td>
+                                    <td>
+
+                                        <button class="btn btn-info" data-toggle="modal" data-target="#book-<?= $row['id'] ?>"><i class="fa fa-pencil"></i></button>
+                                        <button class="btn btn-warning"><i class="fa fa-pencil"></i></button>
+                                        <button class="btn btn-danger"><i class="fa fa-trash-o"></i></button>
+                                    </td>
+
 
                                 </tr>
                             <?php
@@ -68,6 +73,75 @@ require_once 'header.php';
     </div>
 
 </div>
+
+<?php
+$result = mysqli_query($con, "SELECT * FROM `books`");
+while ($row = mysqli_fetch_assoc($result)) {
+
+?>
+    <div class="modal fade" id="book-<?= $row['id'] ?>" tabindex="-1" role="dialog" aria-labelledby="modal-info-label">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header state modal-info">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                    <h4 class="modal-title" id="modal-info-label"><i class="fa fa-book"></i> Book Info </h4>
+                </div>
+                <div class="modal-body">
+                    <table class="table table-bordered" >
+                        <tr>
+                            <th>Book-Name</th>
+                            <td><?= $row['book_name'] ?></td>
+
+                        </tr>
+                        <tr>
+                            <th>Book Image</th>
+                            <td><img src="../images/books/<?= $row['book_image'] ?>" width="100px" alt=""></td>
+
+                        </tr>
+                        <tr>
+                            <th>Publication Name</th>
+                            <td><?= $row['book_publication_name'] ?></td>
+
+                        </tr>
+                        <tr>
+                            <th>Author Name</th>
+                            <td><?= $row['book_author_name'] ?></td>
+
+                        </tr>
+                        <tr>
+                            <th>Purchase Date</th>
+                            <td><?= date('d-M-Y', strtotime($row['book_purchase_date'])) ?></td>
+
+                        </tr>
+                        <tr>
+                            <th>Book Price</th>
+                            <td><?= $row['book_price'] ?></td>
+
+                        </tr>
+                        <tr>
+                            <th>Book Quantity</th>
+                            <td><?= $row['book_qty'] ?></td>
+
+                        </tr>
+                        <tr>
+                            <th>Available Quantity</th>
+                            <td><?= $row['available_qty'] ?></td>
+
+                        </tr>
+                    </table>
+                </div>
+                <div class="modal-footer">
+
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<?php
+}
+?>
+
 <?php
 require_once 'footer.php';
 ?>
